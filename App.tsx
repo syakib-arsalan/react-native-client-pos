ad8e22c05b96e054d68b7fdb68bb4c30d3f11a4c
Syakib-Arsalan74 <syakibgithub@gmail.com>
2025-12-07 16:14:23 +0700

    penambahan splash screen dengan bootsplash

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
-
+import { useEffect} from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StackNavigator } from './src/navigation/StackNavigator';
+import BootSplash from 'react-native-bootsplash';
+import { AuthProvider } from './src/navigation/AuthContext';
 
 function App() {
+  useEffect(() => {
+    const init = async () => {};
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+    });
+  }, []);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar backgroundColor={isDarkMode ? 'black' : 'white'} barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <StackNavigator />
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <StatusBar
+          backgroundColor={isDarkMode ? 'black' : 'white'}
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        />
+        <StackNavigator />
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 }
 
